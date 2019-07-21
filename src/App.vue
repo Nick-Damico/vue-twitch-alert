03a3e91b5c3e6e3de10f926923912f07a864aa45
Nick-Damico <nickalan82@icloud.com>
2019-07-21 17:44:12 -0400

    Fetching TwitchAPI on mount of App component.

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,39 @@
 </template>
 
 <script>
+import axios from 'axios'
 import NavigationBar from './components/NavigationBar'
 import NowPlaying from './components/NowPlaying'
+// Api Access Keys
+const API_KEY = process.env.VUE_APP_SECRET
+const API_CLIENT_ID = process.env.VUE_APP_CLIENT_ID
 
 export default {
   name: 'app',
   components: {
     NavigationBar,
     NowPlaying
+  },
+  data() {
+    return {
+      streamers: []
+    }
+  },
+  mounted() {
+    axios.get('https://api.twitch.tv/helix/streams?first=20', {
+      headers: {
+        'Client-ID': API_CLIENT_ID
+      },
+      params: {
+        client_id: API_CLIENT_ID
+      }
+    })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
 }
 </script>
