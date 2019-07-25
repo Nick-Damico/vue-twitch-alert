1cebced10714495698cbc55c1bb7815b59d6b529
Nick-Damico <nickalan82@icloud.com>
2019-07-24 22:27:14 -0400

    Format NavigationBar.vue with bootstrap-vue components

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,39 +1,39 @@
 <template lang="html">
-  <div id="navigationBar" class="container">
-    <nav class="navbar navbar-dark">
-      <span class="navbar-brand mb-0 h1">TwitchAlert</span>
-      <div>
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="#">AlertIcon</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Icon<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">email@twitchalert.com</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">StreamerAlert</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template slot="button-content"><em>User</em></template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
+
 </template>
 
 <script>
-export default {
-  name: 'NavigationBar'
-}
+
+  export default {
+    name: 'NavigationBar'
+  }
+
 </script>
 
 <style lang="css" scoped>
-  #navigationBar {
-    background-color: black;
-  }
-  ul {
-    display: flex;
-    flex-direction: row;
-  }
-  ul li:nth-child(-n+2) {
-    margin-right: 8px;
-  }
+
 </style>
