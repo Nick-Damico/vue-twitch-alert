f9fda00ab422d053e5a06546a8079b61e2fff503
Nick-Damico <nickalan82@icloud.com>
2019-07-28 21:15:01 -0400

    styled NowPlaying Component

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -1,16 +1,18 @@
 <template lang="html">
   <div v-if="streamer" id="nowPlaying">
-    <div class="frow centered">
-      <div class="col-md-2-3">
-        <div class="video-preview">
-          <img :src="thumbnailUrl" />
+    <div class="wrapper">
+      <div class="frow gutters centered items-stretch">
+        <div class="col-md-1-2">
+          <div class="video-preview">
+            <img :src="thumbnailUrl" />
+          </div>
         </div>
-      </div>
-      <div class="col-md-1-3">
-        <div class="video-details">
-          <h4 class="video-details__title">{{ this.streamer.user_name }}</h4>
-          <p>{{ this.streamer.title }}</p>
-          <b-button href="#" variant="primary">View Streamer</b-button>
+        <div class="col-md-1-2 video-details__col">
+          <div class="video-details">
+            <h4 class="video-details__title">{{ this.streamer.user_name }}</h4>
+            <p class="video-details__text">{{ this.streamer.title }}</p>
+            <b-button class="video-details__btn" href="#" variant="primary">Watch Stream</b-button>
+          </div>
         </div>
       </div>
     </div>
@@ -33,17 +35,36 @@
 <style lang="css" scoped>
 
   #nowPlaying {
+    padding: 30px;
     background-color: #6441a4;
-    height: 100%;
-    min-height: 60vh;
   }
 
   .video-preview {
     text-align: center;
   }
 
+  .video-details__col {
+    max-width: 400px;
+  }
+
   .video-details {
-    align-self: flex-start;
-    text-align: left;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+
+  .video-details__title {
+    color: var(--text-mint);
+    margin-bottom: 8px;
+  }
+
+  .video-details__text {
+    color: var(--text-white);
+  }
+
+  .video-details__btn {
+    display: flex;
+    margin-top: auto;
+    width: 100%;
   }
 </style>
