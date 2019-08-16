7d5da62e6e6ce3e90b7b4d0c05a50ed753a559ee
Nick-Damico <nickalan82@icloud.com>
2019-08-15 23:00:49 -0400

    Update package.js, remove bootstrap dependencies.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import AuthHandler from './components/AuthHandler'
 import LandingPage from './components/LandingPage'
 import store from './store/index'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'frow/dist/frow.min.css'
 import './app.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
-Vue.use(BootstrapVue)
 
 const router = new VueRouter({
   mode: 'history',
