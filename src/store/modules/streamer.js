5b9f973cb52bf82e1294624ca15e972d7add8460
Nick-Damico <nickalan82@icloud.com>
2019-09-08 10:49:17 -0400

    Setup Vuex State for fetching and storing Streamers from api response

--- src/store/modules/streamer.js
+++ src/store/modules/streamer.js
@@ -1,22 +1,32 @@
 import { router } from '../../main'
+import twitch_api from "../../apis/twitch";
 
 const state = {
-  selectedStreamer: null
+  selectedStreamer: null,
+  streamers: []
 };
 
 const getters = {
-  selectedStreamer: state => state.selectedStreamer
+  selectedStreamer: state => state.selectedStreamer,
+  allStreamers: state => state.streamers
 };
 
 const actions = {
   setStreamer({ commit }, streamer) {
     commit("SET_SELECTED_STREAMER", streamer)
+  },
+  async fetchTop20({ commit }) {
+    const streamersResponse = await twitch_api.fetchTop20Streamers()
+    commit("SET_STREAMERS", streamersResponse.data.data)
   }
 };
 
 const mutations = {
   SET_SELECTED_STREAMER: (state, streamer) => {
     state.selectedStreamer = streamer;
+  },
+  SET_STREAMERS: (state, streamers) => {
+    state.streamers = [...state.streamers, streamers]
   }
 };
 
