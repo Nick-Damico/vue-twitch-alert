02deeb5b8a4c26ca6f15f723e569f4cf4e8c836e
Nick-Damico <nickalan82@icloud.com>
2019-10-26 09:02:26 -0400

    sub menu for following options activate on click.

--- src/components/Following.vue
+++ src/components/Following.vue
@@ -7,7 +7,12 @@
       </h2>
     </div>
     <div class="ui item menu inverted following__menu" :class="menuKlasses">
-      <a id="menu__item" v-for="item in menuItems" class="item" :class="{active: isActive(item)}">{{ item }}</a>
+      <a
+        v-for="item in menuItems"
+        v-on:click="onSelect($event)"
+        class="item menu__item"
+        :class="{active: isActive(item)}">{{ item }}
+      </a>
     </div>
     <StreamerList :streamers="liveFollowers" />
   </div>
@@ -15,38 +20,50 @@
 </template>
 
 <script>
+import StreamerList from './StreamerList'
+import {
+  mapGetters,
+  mapActions
+} from 'vuex'
 
-  import StreamerList from './StreamerList'
-  import { mapGetters, mapActions } from 'vuex'
-
-  export default {
-    name: 'Followers',
-    data: () => {
-      return {
-        menuItems: ['Live', 'Offline', 'All', 'Recent VODS'],
-        menuKlasses: ['four']
-      }
+export default {
+  name: 'Followers',
+  data: () => {
+    return {
+      menuItems: ['Live', 'Offline', 'All', 'Recent VODS'],
+      menuKlasses: ['four']
+    }
+  },
+  components: {
+    StreamerList
+  },
+  computed: {
+    ...mapGetters({
+      liveFollowers: 'getFollowers',
+      offlineFollowers: 'getOffline'
+    })
+  },
+  methods: {
+    ...mapActions(['fetchFollowed']),
+    isActive: item => {
+      return item === 'Live'
     },
-    components: {
-      StreamerList
-    },
-    computed: {
-      ...mapGetters({
-        liveFollowers: 'getFollowers',
-        offlineFollowers: 'getOffline'
+    onSelect: (event) => {
+      const links = [...document.querySelectorAll('.menu__item')]
+      const { target } = event
+      links.filter(link => {
+        if (link === target) {
+          link.classList.add('active')
+        } else {
+          link.classList.remove('active')
+        }
       })
-    },
-    methods: {
-      ...mapActions(['fetchFollowed']),
-      isActive: item => {
-        return item === 'Live'
-      }
-    },
+    }
+  },
     created() {
       this.fetchFollowed()
     }
   }
-
 </script>
 
 <style lang="css" scoped>
@@ -58,7 +75,7 @@
     font-weight: 300 !important;
   }
 
-  #menu__item.active {
+  .menu__item.active {
     color: var(--text-black) !important;
     background: #a8ff78 !important;
     background: -webkit-linear-gradient(to right, #78ffd6, #a8ff78) !important;
