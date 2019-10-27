b5c96eb50d6ff1777a81133fc8e3b110ea83163e
Nick-Damico <nickalan82@icloud.com>
2019-10-27 11:29:31 -0400

    Render Streamer Item either Live or Offline version based on type.

--- src/components/StreamerListItem.vue
+++ src/components/StreamerListItem.vue
@@ -1,9 +1,14 @@
 <template lang="html">
 
   <div class="eight wide tablet five wide computer column">
-    <div class="streamer-card" @click="onClick">
+    <div v-if="typeIsLive" class="streamer-card" @click="onClick">
       <img class="ui fluid image" :src="thumbnailUrl" alt="">
       <h5 class="streamer-title">{{ this.streamer.title }}</h5>
+    </div>
+
+    <div v-else-if="typeIsOffline" class="streamer-card" @click="onClick">
+      <img class="ui fluid image" :src="this.streamer.offline_image_url" alt="">
+      <h5 class="streamer-title">{{ this.streamer.display_name }}</h5>
     </div>
   </div>
 
@@ -15,10 +20,22 @@
 
   export default {
     name: 'StreamerListItem',
-    props: ['streamer'],
+    props: {
+      streamer: {
+        type: Object,
+        default: 'Live'
+      },
+      type: String
+    },
     computed: {
       thumbnailUrl() {
         return this.streamer.thumbnail_url.replace(/{width}/, 740).replace(/{height}/, 416)
+      },
+      typeIsLive() {
+        return this.type === 'Live'
+      },
+      typeIsOffline() {
+        return this.type === 'Offline'
       }
     },
     methods: {
